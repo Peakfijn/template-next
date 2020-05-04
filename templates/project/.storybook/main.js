339fd6ccba7254e9abde6a07d61646b8ac52aec4
Lars Bekhof <larsbekhof@gmail.com>
2020-05-04 13:17:06 +0200

    fix(template/project): changed storybook so it uses typescript

--- templates/project/.storybook/main.js
+++ templates/project/.storybook/main.js
@@ -1,4 +1,22 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.(js|ts)x?'],
+	webpackFinal: async config => {
+		config.module.rules.push({
+			test: /\.(ts|tsx)$/,
+			use: [
+				{
+					loader: require.resolve('ts-loader'),
+					options: { compilerOptions: { noEmit: false, jsx: 'react' } },
+				},
+				{
+					loader: require.resolve('react-docgen-typescript-loader'),
+				},
+			],
+		});
+
+		config.resolve.extensions.push('.ts', '.tsx');
+
+		return config;
+	},
+  stories: ['../stories/**/*.stories.(ts|tsx)'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
 };
